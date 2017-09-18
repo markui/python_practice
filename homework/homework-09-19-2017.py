d380d1ed84f658b6be560bb8a19aa15258a4629f
Mark Kim <viking617617@gmail.com>
2017-09-19 00:51:36 +0900

    added homework-09-19-2017.py, bs4

---
+++ homework/homework-09-19-2017.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('http://comic.naver.com/webtoon/list.nhn?titleId=651673')
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+webtoon_titles = [
+					item.select_one('a').text
+					for item 
+					in soup.select('td.title')
+				]
+
+
+print(webtoon_titles)
